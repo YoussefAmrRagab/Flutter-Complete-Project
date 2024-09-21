1ee085524227541671aaef3ec552758f1143aac1
Youssef Amr <youssefamr2244@gmail.com>
2024-09-21 23:47:21 +0300

    - feat(login) ✨ Added login screen.

--- lib/features/onboarding/ui/onboarding_screen.dart
+++ lib/features/onboarding/ui/onboarding_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_project/core/helpers/extensions.dart';
+import 'package:flutter_complete_project/core/routing/routes.dart';
+import 'package:flutter_complete_project/core/widgets/app_text_button.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../core/theming/styles.dart';
 import 'widgets/doctor_image_and_text.dart';
 import 'widgets/doc_logo_and_Name.dart';
-import 'widgets/get_started_button.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -15,30 +16,34 @@ class OnboardingScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 30.h),
-            child: Column(
-              children: [
-                const DocLogoAndName(),
-                30.0.margin,
-                const DoctorImageAndText(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.w),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
-                        textAlign: TextAlign.center,
-                        style: TextStyles.font13grayRegular,
-                      ),
-                      30.0.margin,
-                      const GetStartedButton(),
-                    ],
-                  ),
-                )
-              ],
-            ),
+          padding: EdgeInsets.symmetric(vertical: 30.h),
+          child: Column(
+            children: [
+              const DocLogoAndName(),
+              30.0.verticalSpace,
+              const DoctorImageAndText(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
+                      textAlign: TextAlign.center,
+                      style: TextStyles.font13GrayRegular,
+                    ),
+                    30.0.verticalSpace,
+                    AppTextButton(
+                      buttonText: 'Get Started',
+                      textStyle: TextStyles.font16WhiteSemiBold,
+                      onPressed: () {
+                        context.pushReplacementNamed(Routes.loginScreen);
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
